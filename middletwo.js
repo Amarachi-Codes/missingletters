8a0dd4de4fde0a099bd6b7940229d1762c7d3843
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-02 22:26:28 +0100

    middletwo solved

--- middletwo.js
+++ middletwo.js
@@ -1,9 +1,19 @@
-String-1 -- middleTwo
-Given a string of even length, return a string made of the middle two chars, so the string "string" 
-yields "ri". The string length will be at least 2.
+// String-1 -- middleTwo
+// Given a string of even length, return a string made of the middle two chars, so the string "string" 
+// yields "ri". The string length will be at least 2.
 
-Examples
+// Examples
 
-middleTwo('string') →
-middleTwo('code') →
-middleTwo('Practice') →
+// middleTwo('string') →
+// middleTwo('code') →
+// middleTwo('Practice') →
+
+function middleTwo(str) {
+    let len = str.length / 2
+    let end = len + 1
+    let start = len - 1
+    if (str.length % 2 === 0 && str.length >= 2) {
+        let substr = str.slice(start, end)
+        return substr
+    } return ''
+} console.log(middleTwo('string'));
