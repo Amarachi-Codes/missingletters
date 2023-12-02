765f8f8cd4469939141331f820de3ab490e44283
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-02 21:36:31 +0100

    missingletter

---
+++ missingletter.js
@@ -0,0 +1,7 @@
+// Missing letter
+// Write a method that takes an array of consecutive (increasing) letters as input and returns the missing letter in the array.
+
+// You will always get a valid array. And it will always be exactly one letter missing. The length of the array will always be at least 2. The array will always contain letters in only one case.
+
+// E.g ['a','b','c','d','f'] =>'e'
+// ['O','Q','R','S']=>'P'
