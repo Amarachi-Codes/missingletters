cf8006dfb590e1873e4f05bc8657ae01039733c6
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2023-12-02 22:57:46 +0100

    solved

--- withoutx.js
+++ withoutx.js
@@ -7,3 +7,18 @@
 // withoutX('xHix') → Hi
 // withoutX('xHi') → Hi
 // withoutX('Hxix') → Hxi
+
+function withoutX(str){
+ let start = str.indexOf(str.charAt(1))
+let end = str.length -1
+if(str.startsWith("x") && str.endsWith("x")){
+   let substr = str.slice(start,end) 
+   return substr
+}if(str.startsWith("x")){
+    let frontx = str.slice(start) 
+    return frontx
+ }if(str.endsWith("x")){
+    let endx = str.slice(0,end) 
+    return endx
+ } return str
+}console.log(withoutX('chxiu'));
